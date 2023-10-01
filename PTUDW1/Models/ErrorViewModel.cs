cebbd19f0f0aef9592c433e57e9e2c07c0ac2bfd
admin <admin@LAPTOP-VCM50FG8>
2023-10-02 02:21:54 +0700

    Add project files.

---
+++ PTUDW1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PTUDW1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
