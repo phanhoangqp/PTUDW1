d2fe847a97e6cb6b840f551fc96e431b3abdeb36
phanhoangqp <146301204+phanhoangqp@users.noreply.github.com>
2023-10-07 14:03:48 +0700

    Add files via upload

---
+++ Components/PostComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTUDW1.Models;
+namespace PTUDW1.Components
+{
+    [ViewComponent(Name = "Post")]
+    public class PostComponent : ViewComponent
+    {
+        private readonly DataContext _context;
+        public PostComponent(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var listofPost = (from m in _context.Posts
+                              where (m.IsActive == true) && (m.Status == 1)
+                              orderby m.PostID descending
+                              select m).ToList();
+            return await Task.FromResult((IViewComponentResult)View("Default", listofPost));
+        }
+    }
+}
---
+++ Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace PTUDW1.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { }
+        public DbSet<Menu> Menus { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
